05ef34df31616eb424aed732e9a851496ec9154a
Ed Forrest <forrestedw@gmail.com>
2020-07-06 12:10:22 +0100

    convert kilograms to lbs

--- src/Unit ConvertionClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Forrestedw\Unit Convertion;
-
-class Unit ConvertionClass
-{
-}
---
+++ src/Weight.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Forrestedw\UnitConversions;
+
+class Weight
+{
+    /**
+     * @var float
+     */
+    private $kilograms;
+
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+
+        $this->kilograms = $kilograms;
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.204623;
+    }
+}
