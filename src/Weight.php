fb07303cf73c6b640939f83a1d9b5fa1f10c9b00
Ed Forrest <forrestedw@gmail.com>
2020-07-06 16:46:22 +0100

    adds convertion from grams and to oz

--- src/Weight.php
+++ src/Weight.php
@@ -14,6 +14,11 @@ class Weight
         return new static($kilograms);
     }
 
+    public static function fromGrams(float $grams): self
+    {
+        return new static($grams / 1000);
+    }
+
     public function __construct(float $kilograms)
     {
         $this->kilograms = $kilograms;
@@ -22,5 +27,10 @@ class Weight
     public function toLbs(): float
     {
         return $this->kilograms * 2.204623;
+    }
+
+    public function toOz(): float
+    {
+        return $this->kilograms * 35.274;
     }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -14,4 +14,20 @@ class WeightTest extends TestCase
 
         $this->assertEquals(220.4623, $lbs);
     }
+
+    /** @test */
+    public function it_can_convert_grams_to_lbs()
+    {
+        $lbs = Weight::fromGrams(100)->toLbs();
+
+        $this->assertEquals(round(0.220462,6), round($lbs,6));
+    }
+
+    /** @test */
+    public function it_can_convert_grams_to_oz()
+    {
+        $oz = Weight::fromGrams(100)->toOz();
+
+        $this->assertEquals(round(3.5274,4), round($oz,4));
+    }
 }
